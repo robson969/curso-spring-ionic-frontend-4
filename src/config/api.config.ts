c19c796a5b1ace79421ce0cb274d5270de33c1a6
robson969 <robson969@gmail.com>
2019-06-03 22:35:52 -0300

    alterações no config do projeto

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "https://spring-ionic-nelio.herokuapp.com",
-    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic"
+    baseUrl: "https://curso-spring-ionic-robson.herokuapp.com",
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-robson"
 }
